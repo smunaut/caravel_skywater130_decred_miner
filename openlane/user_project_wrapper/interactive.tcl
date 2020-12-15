656bf735d13576d340bdb0d7497107e9e11646e1
Sylvain Munaut <tnt@246tNt.com>
2020-12-15 23:05:19 +0100

    Attempted fix to user_project_wrappr PDN
    Signed-off-by: Sylvain Munaut <tnt@246tNt.com>

--- openlane/user_project_wrapper/interactive.tcl
+++ openlane/user_project_wrapper/interactive.tcl
@@ -27,32 +27,8 @@ manual_macro_placement f
 
 #run_placement
 
-set ::env(_SPACING) 1.7
-set ::env(_WIDTH) 3
-
-set power_domains [list {vccd1 vssd1} {vccd2 vssd2} {vdda1 vssa1} {vdda2 vssa2}]
-
-set ::env(_VDD_NET_NAME) vccd1
-set ::env(_GND_NET_NAME) vssd1
-set ::env(_V_OFFSET) 14
-set ::env(_H_OFFSET) $::env(_V_OFFSET)
-set ::env(_V_PITCH) 180
-set ::env(_H_PITCH) 180
-set ::env(_V_PDN_OFFSET) 0
-set ::env(_H_PDN_OFFSET) 0
-
-foreach domain $power_domains {
-	set ::env(_VDD_NET_NAME) [lindex $domain 0]
-	set ::env(_GND_NET_NAME) [lindex $domain 1]
-	gen_pdn
-
-	set ::env(_V_OFFSET) \
-	[expr $::env(_V_OFFSET) + 2*($::env(_WIDTH)+$::env(_SPACING))]
-	set ::env(_H_OFFSET) \
-	[expr $::env(_H_OFFSET) + 2*($::env(_WIDTH)+$::env(_SPACING))]
-	set ::env(_V_PDN_OFFSET) [expr $::env(_V_PDN_OFFSET)+6*$::env(_WIDTH)]
-	set ::env(_H_PDN_OFFSET) [expr $::env(_H_PDN_OFFSET)+6*$::env(_WIDTH)]
-}
+exec -ignorestderr openroad -exit $script_dir/gen_pdn.tcl
+set_def $::env(pdn_tmp_file_tag).def
 
 global_routing_or
 detailed_routing
--- openlane/user_project_wrapper/pdn.tcl
+++ openlane/user_project_wrapper/pdn.tcl
@@ -2,7 +2,7 @@
 set ::power_nets $::env(_VDD_NET_NAME)
 set ::ground_nets $::env(_GND_NET_NAME)
 
-pdngen::specify_grid stdcell {
+set stdcell {
     name grid
 	core_ring {
 		met5 {width $::env(_WIDTH) spacing $::env(_SPACING) core_offset $::env(_H_OFFSET)}
@@ -10,12 +10,17 @@ pdngen::specify_grid stdcell {
 	}
 	rails {
 	}
-    straps {
+    connect {{met4 met5}}
+}
+
+if { $::env(_WITH_STRAPS) } {
+	dict append stdcell straps {
 	    met4 {width $::env(_WIDTH) pitch $::env(_V_PITCH) offset $::env(_V_PDN_OFFSET)}
 	    met5 {width $::env(_WIDTH) pitch $::env(_H_PITCH) offset $::env(_H_PDN_OFFSET)}
     }
-    connect {{met4 met5}}
 }
+
+pdngen::specify_grid stdcell $stdcell
 
 pdngen::specify_grid macro {
 	instance "obs_core_obs"
@@ -29,19 +34,18 @@ pdngen::specify_grid macro {
 
 
 pdngen::specify_grid macro {
-    power_pins $::env(_VDD_NET_NAME)
-    ground_pins $::env(_GND_NET_NAME)
-    blockages ""
+    power_pins "$::env(_VDD_NET_NAME) VPWR VDD vdd"
+    ground_pins "$::env(_GND_NET_NAME) VGND VSS gnd"
+    blockages "li1 met1 met2 met3 met4"
     straps { 
     } 
-    connect {}
+    connect {{met4_PIN_ver met5}}
 }
 
-set ::halo 0
+set ::halo 5
 
 # POWER or GROUND #Std. cell rails starting with power or ground rails at the bottom of the core area
 set ::rails_start_with "POWER" ;
 
 # POWER or GROUND #Upper metal stripes starting with power or ground rails at the left/bottom of the core area
 set ::stripes_start_with "POWER" ;
-
